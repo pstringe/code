024e2c03cdf9eb504b1c12555d93c2316a1f384e
Poitier Stringer <stringerpoitier@gmail.com>
2025-10-25 13:08:09 -0700

    0200 0201

--- 02.c
+++ 02.c
@@ -1,1 +1,6 @@
+#include <unistd.h>
 
+int f0000 (int a0000, int a0001) { write(1, &a0000, 1 ); return (a0001); }
+
+//int main  ( ) { return ( f0000(48, 0) ); }
+int main  ( ) { return ( f0000(49, 0) ); }
